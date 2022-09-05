8f21b01caf88ac0cb3b88ddd95db4b06f0b5fba4
chaotic <284042310@qq.com>
2022-09-06 00:01:26 +0800

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.io.Console;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    System.out.println("名字");
+    String name= Console.class.getName();
+
     }
 }
